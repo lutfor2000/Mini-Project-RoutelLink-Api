258e34bb3fc4e89c289caf094af8330f89e2d681
Lutfor Rhaman <lutfar199296@gmail.com>
2025-02-26 13:09:35 +0600

    Vue project,ajax, api call, link, load data

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ContactView from '@/views/ContactView.vue'
+import BlogView from '@/views/BlogView.vue'
+import SingleView from '@/views/SingleView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView,
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: BlogView,
+    },
+
+    {
+      path: '/post/:id',
+      name: 'post',
+      component: SingleView,
+    },
+   
+  ],
+})
+
+export default router
